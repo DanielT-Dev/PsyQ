f4b9f1b50daf30c68de6bcf85e4843fa3f803a99
DanielT-Dev <danieltrusca1@outlook.com>
2026-02-26 12:10:20 +0200

    Create sign-up page view and template + link to home page button

--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'core/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
